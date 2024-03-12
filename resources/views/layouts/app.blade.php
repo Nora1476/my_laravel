8390496aa9c5226d0ad2a80336eb5572d87365b8
Nora1476 <74698994+Nora1476@users.noreply.github.com>
2024-03-12 19:27:07 +0900

    240312

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,48 +21,14 @@
 <body>
     <div id="app">
         <nav class="navbar">
-            <div class="container">
-                {{-- <a href="/">{{ setting('site.title') }}</a> --}}
-                <a href="/"> <img src="{{ Voyager::image(setting('site.logo')) }}" alt="logo"
-                        style="width: 80px; height:80px;"></a>
-
+            {{-- <a href="/">{{ setting('site.title') }}</a> --}}
+            <a href="/"> <img src="{{ Voyager::image(setting('site.logo')) }}" alt="logo" style="width: 80px; height:80px;"></a>
+            <div class="pc_ver">
                 {!! menu('main', 'layouts/mymenu') !!}
-                <ul>
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
+            </div>
 
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-
-
+            <div class="mo_ver">
+                {!! menu('main', 'layouts/mymenu') !!}
             </div>
         </nav>
 
--- resources/views/layouts/mymenu.blade.php
+++ resources/views/layouts/mymenu.blade.php
@@ -1,57 +1,90 @@
 <ul class="gnb">
 
-    @php
+  @php
 
-        if (Voyager::translatable($items)) {
-            $items = $items->load('translations');
-        }
+  if (Voyager::translatable($items)) {
+  $items = $items->load('translations');
+  }
 
-    @endphp
+  @endphp
 
-    @foreach ($items as $item)
-        @php
+  @foreach ($items as $item)
+  @php
 
-            $originalItem = $item;
-            if (Voyager::translatable($item)) {
-                $item = $item->translate($options->locale);
-            }
+  $originalItem = $item;
+  if (Voyager::translatable($item)) {
+  $item = $item->translate($options->locale);
+  }
 
-            $isActive = null;
-            $styles = null;
-            $icon = null;
+  $isActive = null;
+  $styles = null;
+  $icon = null;
 
-            // Background Color or Color
-            if (isset($options->color) && $options->color == true) {
-                $styles = 'color:' . $item->color;
-            }
-            if (isset($options->background) && $options->background == true) {
-                $styles = 'background-color:' . $item->color;
-            }
+  // Background Color or Color
+  if (isset($options->color) && $options->color == true) {
+  $styles = 'color:' . $item->color;
+  }
+  if (isset($options->background) && $options->background == true) {
+  $styles = 'background-color:' . $item->color;
+  }
 
-            // Check if link is current
-            if (url($item->link()) == url()->current()) {
-                $isActive = 'active';
-            }
+  // Check if link is current
+  if (url($item->link()) == url()->current()) {
+  $isActive = 'active';
+  }
 
-            // Set Icon
-            if (isset($options->icon) && $options->icon == true) {
-                $icon = '<i class="' . $item->icon_class . '"></i>';
-            }
+  // Set Icon
+  if (isset($options->icon) && $options->icon == true) {
+  $icon = '<i class="' . $item->icon_class . '"></i>';
+  }
 
-        @endphp
+  @endphp
 
-        <li class="{{ $isActive }}">
-            <a href="{{ url($item->link()) }}" target="{{ $item->target }}" style="{{ $styles }}">
-                {!! $icon !!}
-                <span>{{ $item->title }}</span>
-            </a>
-            @if (!$originalItem->children->isEmpty())
-                @include('voyager::menu.default', [
-                    'items' => $originalItem->children,
-                    'options' => $options,
-                ])
-            @endif
-        </li>
-    @endforeach
+  <li class="{{ $isActive }} nav-item">
+    <a href="{{ url($item->link()) }}" target="{{ $item->target }}" style="{{ $styles }}">
+      {!! $icon !!}
+      <span>{{ $item->title }}</span>
+    </a>
+    @if (!$originalItem->children->isEmpty())
+    @include('voyager::menu.default', [
+    'items' => $originalItem->children,
+    'options' => $options,
+    ])
+    @endif
+  </li>
+
+
+  @endforeach
+  @guest
+  @if (Route::has('login'))
+  <li class="nav-item">
+    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+  </li>
+  @endif
+
+  @if (Route::has('register'))
+  <li class="nav-item">
+    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+  </li>
+  @endif
+  @else
+  <li class="nav-item dropdown">
+    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+      aria-haspopup="true" aria-expanded="false" v-pre>
+      {{ Auth::user()->name }}
+    </a>
+
+    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+      <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+        {{ __('Logout') }}
+      </a>
+
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf
+      </form>
+    </div>
+  </li>
+  @endguest
 
 </ul>
