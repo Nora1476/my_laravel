1073f6997008bc3607050850d81fae904592e208
Nora1476 <nada70979@gmail.com>
2024-03-12 13:57:34 +0900

    240312

--- resources/views/comment.blade.php
+++ resources/views/comment.blade.php
@@ -1,11 +1,16 @@
 <?php $page = App\Models\Comment::first(); ?>
 
+
 @extends('layouts.app')
 
 @section('content')
     @can('browse', $page)
         <div class="comment">
             You can browse pages
+        </div>
+    @else
+        <div class="comment">
+            You do not have access to this pages
         </div>
     @endcan
 @endsection
